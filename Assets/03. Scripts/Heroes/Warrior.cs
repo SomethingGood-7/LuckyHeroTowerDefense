2e3bdbf858c4fd8e9c1f541e336bc897d080fd27
SomethingGood-7 <ghjpower0@naver.com>
2025-08-24 17:25:45 +0900

    update change

--- Assets/03. Scripts/Heroes/Warrior.cs
+++ Assets/03. Scripts/Heroes/Warrior.cs
@@ -1,3 +1,5 @@
+﻿using System;
+using UniRx;
 using UnityEngine;
 
 public class Warrior : HeroUnit
@@ -7,8 +9,40 @@ public class Warrior : HeroUnit
 
     protected override void StartAttack(Transform target)
     {
-        _currentAttackClipId = Random.Range(0, 2) == 0 ? Attack1ClipId : Attack2ClipId;
+        _currentAttackClipId = UnityEngine.Random.Range(0, 2) == 0 ? Attack1ClipId : Attack2ClipId;
         
         base.StartAttack(target);
     }
+
+    private void Start()
+    {
+        Player currentPlayer = PlayerManager.Instance.CurrentPlayer;
+        if (currentPlayer != null)
+        {
+            currentPlayer.DamageUp
+                         .Subscribe(UpgradeDamage)
+                         .AddTo(this);
+        }
+
+        if(currentPlayer != null)
+        {
+            currentPlayer.AttackSpeedUp
+                         .Subscribe(UpgradeAttackSpeed)
+                         .AddTo(this);
+        }
+    }
+
+    private void UpgradeDamage(int level)
+    {
+        //_damageAmount += 10 * level;
+        _damageAmount += 20;
+    }
+
+    private void UpgradeAttackSpeed(int level)
+    {
+        //_attackCooldown = Math.Max((float)0.25, _attackCooldown - (float)(0.1 * level));
+        _attackCooldown = Math.Max((float)0.25, _attackCooldown - (float)(0.1));
+    }
+
+
 }
